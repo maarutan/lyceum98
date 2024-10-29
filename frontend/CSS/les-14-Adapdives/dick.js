6c998022046b3e976880a0a947a170cac438d2e0
maaru <maratarzymatov288@gmail.com>
2024-10-30 03:12:23 +0600

    add

--- frontend/CSS/les-14-Adapdives/dick.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("#fuck");
-const dick = document.querySelector(".dick");
-const dickFuck = document.querySelector(".dickFuck");
-
-btn.addEventListener("click", () => {
-	dick.classList.add("is-active");
-	dickFuck.classList.add("none");
-});
